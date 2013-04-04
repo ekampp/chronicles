a80b1dbeb6e81d0d5940fd40e063945151a50fff
Emil Kampp <emil@kampp.me>
2013-04-04 12:23:03 +0100

    Add progress bar to character index.
    This removes the requirement that public and private bio should always
    be present.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -12,10 +12,28 @@ describe Character do
   # Validations
   it { should be_valid }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:public_bio) }
-  it { should validate_presence_of(:private_bio) }
 
   # Associations
   it { should belong_to :user }
 
+  describe "instance methods" do
+    describe "#progress" do
+      subject{ Character.new }
+      context "1/3 done" do
+        before{ subject.name = "Foo" }
+        its(:progress) { should eq 34 }
+      end
+      context "2/3 done" do
+        before{ subject.name = "Foo";
+                subject.public_bio = "Bar" }
+        its(:progress) { should eq 67 }
+      end
+      context "3/3 done" do
+        before{ subject.name = "Foo";
+                subject.public_bio = "Bar";
+                subject.private_bio = "Blob" }
+        its(:progress) { should eq 100 }
+      end
+    end
+  end
 end
